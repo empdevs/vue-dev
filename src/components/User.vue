68abbc1c36111d9f0f8caa9c57951ec0759302cb
haekalarif <haekal.rozikin@kitameraki.com>
2022-11-24 23:35:22 +0700

    first commit

---
+++ src/components/User.vue
@@ -0,0 +1,22 @@
+<template lang="ts">
+    <div>
+        <h1>This is page user</h1>
+    </div>
+</template>
+<script lang="ts">
+export default {
+    name:"User",
+    data:function(){
+        return{
+            
+        }
+    },
+    methods:{},
+    created:function(){
+        undefined
+    }
+}
+</script>
+<style lang="">
+    
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,12 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
-import router from './router'
+import { createApp } from 'vue';
+import App from './App.vue';
+import './registerServiceWorker';
+import router from './router';
+import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/antd.css';
 
-createApp(App).use(router).mount('#app')
+// createApp(App).use(router).mount('#app');
+const app = createApp(App);
+app.use(router);
+app.use(Antd);
+app.mount("#app");
